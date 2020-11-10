8e1dac71a4e6f7a2df5709b6097274355892bc24
xiaofeng <xiaofeng@didiglobal.com>
2020-11-10 18:50:55 +0800

    feat: 增加入口页

--- case-server/src/main/java/com/xiaoju/framework/controller/WebController.java
+++ case-server/src/main/java/com/xiaoju/framework/controller/WebController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
     @RequestMapping("/case/caseList/1")
     public String index(){
         return "index";
