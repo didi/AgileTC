5ae65ec5852b696c84bb0a82a593f671e56ed314
jiangrongrong <jiangrongrong@didichuxing.com>
2021-01-29 11:50:22 +0800

    去掉导入导出xmind用例对xmind jar包的依赖

--- case-server/src/main/java/com/xiaoju/framework/service/impl/FileServiceImpl.java
+++ case-server/src/main/java/com/xiaoju/framework/service/impl/FileServiceImpl.java
@@ -15,14 +15,21 @@ import com.xiaoju.framework.service.FileService;
 import com.xiaoju.framework.util.FileUtil;
 import com.xiaoju.framework.util.TreeUtil;
 import org.apache.commons.io.IOUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
-import org.xmind.core.*;
 
 import javax.annotation.Resource;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.xiaoju.framework.constants.SystemConstant.POINT;
 import static com.xiaoju.framework.constants.XmindConstant.*;
@@ -41,6 +48,8 @@ public class FileServiceImpl implements FileService {
 
     @Resource
     private TestCaseMapper caseMapper;
+
+    private static final Logger log = LoggerFactory.getLogger(FileServiceImpl.class);
 
     @Override
     @Transactional(rollbackFor = Exception.class)
@@ -81,7 +90,7 @@ public class FileServiceImpl implements FileService {
 
             // 导入用例
             File jsonFile = new File((desc + CONTENT_JSON).replace("/", File.separator));
-            CaseCreateReq caseCreateReq = jsonFile.exists() ? buildCaseByJson(req, desc) : buildCaseByXmind(req, desc);
+            CaseCreateReq caseCreateReq = jsonFile.exists() ? buildCaseByJson(req, desc) : buildCaseByXml(req, desc);
             return caseService.insertOrDuplicateCase(caseCreateReq);
         }
         throw new CaseServerException("传入的文件名非法", StatusCode.FILE_IMPORT_ERROR);
@@ -89,49 +98,130 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public ExportXmindResp exportXmindFile(Long id, String userAgent) throws Exception {
-        IWorkbookBuilder builder = Core.getWorkbookBuilder();
-        IWorkbook workbook = builder.createWorkbook();
-        ISheet defSheet = workbook.getPrimarySheet();
-        ITopic rootTopic = defSheet.getRootTopic();
 
+        ExportXmindResp resp = new ExportXmindResp();
+
+        //将用例内容写内容入xml文件
+        Map<String,String> pathMap= createXml(id);
+
+        //压缩文件夹成xmind文件
+        String filePath = pathMap.get("exportPath") + ".xmind";
+        FileUtil.compressZip(pathMap.get("exportPath") ,filePath);
+        // 输出
+        ByteArrayOutputStream byteArrayOutputStream = outPutFile(filePath);
+        resp.setFileName(pathMap.get("exportFileName"));
+        resp.setData(byteArrayOutputStream.toByteArray());
+
+        return resp;
+    }
+
+    private ByteArrayOutputStream outPutFile(String filePath){
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
+        try{
+            InputStream fis = new FileInputStream(filePath);
+            byte[] buffer = new byte[1024];
+            int r = 0;
+            while ((r = fis.read(buffer)) != -1) {
+                bufferedOutputStream.write(buffer, 0, r);
+            }
+            fis.close();
+            bufferedOutputStream.flush();
+            IOUtils.closeQuietly(bufferedOutputStream);
+            IOUtils.closeQuietly(byteArrayOutputStream);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            IOUtils.closeQuietly(bufferedOutputStream);
+            IOUtils.closeQuietly(byteArrayOutputStream);
+            throw new CaseServerException("导出失败", StatusCode.FILE_EXPORT_ERROR);
+        }
+        return byteArrayOutputStream;
+    }
+
+    //根据用例生成xml文件
+    private Map<String,String> createXml(Long id)
+    {
         TestCase testCase = caseMapper.selectOne(id);
         if (testCase == null || StringUtils.isEmpty(testCase.getCaseContent())) {
             throw new CaseServerException("用例不存在或者content为空", StatusCode.FILE_EXPORT_ERROR);
         }
+        //写入文件内容
+        Document document = createDocument(testCase);
+
+        //将内容写入xml
+        String path = writeXml(document, testCase);
+        Map<String,String> pathMap = new HashMap<>();
+        pathMap.put("exportPath",path);
+        pathMap.put("exportFileName",testCase.getTitle() + ".xmind");
+        return pathMap;
+    }
+
+    //拼接xml内容
+    private Document createDocument(TestCase testCase){
+        // 1、创建document对象
+        Document document = DocumentHelper.createDocument();
+        // 2、创建根节点root
+        Element root = document.addElement("xmap-content");
+        // 3、生成子节点及子节点内容
+        Element sheet = root.addElement("sheet")
+                .addAttribute("id",ZEN_ROOT_VERSION)
+                .addAttribute("modified-by",XMIND_MODIFIED_VERSION)
+                .addAttribute("theme",XMIND_THEME_VERSION)
+                .addAttribute("timestamp",XMIND_CREATED_VERSION);
 
         JSONObject rootObj = JSON.parseObject(testCase.getCaseContent()).getJSONObject(ROOT);
-        rootTopic.setTitleText(rootObj.getJSONObject(DATA).getString("text"));
-        rootTopic.setFolded(false);
+        Element topic = sheet.addElement("topic")
+                .addAttribute("id",rootObj.getJSONObject(DATA).getString("id"))
+                .addAttribute("modified-by","didi")
+                .addAttribute("timestamp",rootObj.getJSONObject(DATA).getString("created"));
 
-        TreeUtil.exportData(rootObj.getJSONArray("children"), workbook, rootTopic);
+        Element title = topic.addElement("title");
+        String text = rootObj.getJSONObject(DATA).getString("text");
+        text = text.replace("<","&lt;");
+        text = text.replace(">","&gt;");
+        title.setText(text);
+        TreeUtil.exportDataToXml(rootObj.getJSONArray("children"), topic);
+        return document;
+    }
 
-        defSheet.setThemeId(XMIND_THEME_VERSION);
-        defSheet.setStyleId(XMIND_THEME_VERSION);
-
-        // http返回的文件名
-        String title = testCase.getTitle() + "_" + System.currentTimeMillis() + "_u.xmind";
-        String fileName = new String(title.getBytes(), StandardCharsets.UTF_8);
-        workbook.save(fileName);
-
-        // 服务器本地存有的x-mind文件
-        String filePath = new File("").getCanonicalPath() + "/" + fileName;
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
-        InputStream fis = new FileInputStream(filePath);
-        byte[] buffer = new byte[1024];
-        int r = 0;
-        while ((r = fis.read(buffer)) != -1) {
-            bufferedOutputStream.write(buffer, 0, r);
+    //创建要写入的文件夹
+    private String creteFolder(TestCase testCase){
+        String filePath = "";
+        try{
+            filePath = new File("").getCanonicalPath();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        fis.close();
-        bufferedOutputStream.flush();
-        IOUtils.closeQuietly(bufferedOutputStream);
-        IOUtils.closeQuietly(byteArrayOutputStream);
+        String folderName = testCase.getTitle().replace(" ","")+ "_" + System.currentTimeMillis();
+        String desPath = filePath + TEMP_FOLDER_EXPORT + folderName;
+        File pathFile = new File(desPath);
+        if (!pathFile.exists()) {
+            pathFile.mkdirs();
+        }
+        return  desPath;
+    }
 
-        ExportXmindResp resp = new ExportXmindResp();
-        resp.setFileName(fileName);
-        resp.setData(byteArrayOutputStream.toByteArray());
-        return resp;
+    private String writeXml(Document document, TestCase testCase){
+        String path = creteFolder(testCase);
+        // 设置生成xml的格式
+        OutputFormat format = OutputFormat.createPrettyPrint();
+        // 设置编码格式
+        format.setEncoding("UTF-8");
+        String targetPath = path  + "/content.xml";
+        File xmlFile = new File(targetPath);
+        try {
+            XMLWriter writer = new XMLWriter(new FileOutputStream(xmlFile), format);
+            // 设置是否转义，默认使用转义字符
+            writer.setEscapeText(false);
+            writer.write(document);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("生成文件夹和content.xml失败");
+        }
+        return path;
     }
 
     private CaseCreateReq buildCaseByJson(FileImportReq request, String fileName) {
@@ -148,24 +238,29 @@ public class FileServiceImpl implements FileService {
         return buildCaseCreateReq(request, jsonArray);
     }
 
-    public CaseCreateReq buildCaseByXmind(FileImportReq request, String fileName) throws Exception {
-        IWorkbookBuilder builder = Core.getWorkbookBuilder();
-        IWorkbook workbook = builder.loadFromPath(fileName);
-        // 打开x-mind文件，先获取主sheet，在获取根topic
-        ISheet defSheet = workbook.getPrimarySheet();
-        ITopic rootTopic = defSheet.getRootTopic();
+    //xmind8从content文件读取用例内容
+    public CaseCreateReq buildCaseByXml(FileImportReq request, String fileName) throws Exception {
 
-        //暂不支持x-mind zen版本
-        if (ZEN_ROOT_VERSION.equals(rootTopic.getId())) {
-            throw new CaseServerException("暂不支持Xmind-Zen版本", StatusCode.FILE_IMPORT_ERROR);
-        }
-
+        // 开始读取文件中的xml内容了
         JSONArray jsonArray = new JSONArray();
-        TreeUtil.importData(jsonArray, rootTopic);
-
+        String fileXml = "content.xml";
+        fileName = (fileName + fileXml).replace("/", File.separator);
+        File file = new File(fileName);
+        if(!file.exists())
+            throw new CaseServerException("导入失败，文件不存在", StatusCode.FILE_IMPORT_ERROR);
+        SAXReader reade = new SAXReader();
+        org.dom4j.Document doc = reade.read(file);
+        Element rootElement = doc.getRootElement();
+        List<Element> elementList = rootElement.elements();
+        Element childElement = elementList.get(0);
+        String eleName = childElement.getName();
+        if(eleName.equalsIgnoreCase("sheet"))
+        {
+            jsonArray = TreeUtil.importDataByXml(childElement);
+        }
         return buildCaseCreateReq(request, jsonArray);
-
     }
+
 
     private CaseCreateReq buildCaseCreateReq(FileImportReq request, JSONArray jsonArray) {
         // 构建content
@@ -179,7 +274,11 @@ public class FileServiceImpl implements FileService {
         CaseCreateReq testCase = new CaseCreateReq();
         testCase.setProductLineId(request.getProductLineId());
         testCase.setCreator(request.getCreator());
-        testCase.setRequirementId(request.getRequirementId());
+        if(request.getRequirementId().equals("undefined")) {
+            testCase.setRequirementId("");
+        }else {
+            testCase.setRequirementId(request.getRequirementId());
+        }
         testCase.setProductLineId(request.getProductLineId());
         testCase.setDescription(request.getDescription());
         testCase.setTitle(request.getTitle());
