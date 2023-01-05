1b812aaad34bf600beb1344852b34f2767fd93ba
liqi <1242201319@qq.com>
2023-01-05 12:26:47 +0800

    modify default authority name

--- case-server/src/main/java/com/xiaoju/framework/constants/SystemConstant.java
+++ case-server/src/main/java/com/xiaoju/framework/constants/SystemConstant.java
@@ -13,13 +13,13 @@ public class SystemConstant {
     public final static String COMMA = ",";
 
     public final static Integer IS_DELETE = 1;
-    
+
     public final static Integer NOT_DELETE = 0;
 
     public static final String POINT = ".";
 
     public static final String HTTP_USER_AGENT = "User-Agent";
 
-    public static final String DEFAULT_AUTHORITY_NAME = "ROLE_USER";
+    public static final String DEFAULT_AUTHORITY_NAME = "ROLE_RD";
 
 }
